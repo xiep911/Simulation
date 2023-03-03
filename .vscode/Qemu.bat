8df9fc55de1b42f1438a013a3e3c6593475a8c7f
xp911 <2426073232@qq.cpm>
2023-03-03 11:30:41 +0800

    Verify bat to get more log
    Signed-off-by: xp911 <2426073232@qq.cpm>

--- .vscode/Qemu.bat
+++ .vscode/Qemu.bat
@@ -1,3 +1,3 @@
 @echo off
 copy ..\Build\Ovmf3264\DEBUG_VS2015x86\X64\my*.efi fat
-qemu-system-x86_64w.exe -nic none -serial file:serial.log -pflash .\..\Build\Ovmf3264\DEBUG_VS2015x86\FV\OVMF.fd -hdb fat:rw:fat %*
+qemu-system-x86_64w.exe -pflash .\..\Build\Ovmf3264\DEBUG_VS2015x86\FV\OVMF.fd %*
